0d83ccb5292a0c760b8d490e87a571a5cf324140
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-07-01 17:49:41 +0200

    Add web-service to get single point

--- reportr/views.py
+++ reportr/views.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
 from reportr.models import DBSession, Point
 from geoalchemy2.shape import to_shape
+from pyramid.httpexceptions import HTTPNotFound, HTTPBadRequest
+from sqlalchemy.orm.exc import NoResultFound
 
 
 @view_config(route_name='home', renderer='templates/index.mako')
@@ -16,29 +18,47 @@ def imprint(request):
 @view_config(route_name='points', renderer='json')
 def points(request):
     points = DBSession.query(Point).all()
-
-    points_geojson = []
-    for point in points:
-        # convert from WKB to a Shapely geometry
-        geom = to_shape(point.geom)
-
-        points_geojson.append({
-            'type': 'Feature',
-            'properties': {
-                'id': point.id,
-                'title': point.title,
-                'date': str(point.date)
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [
-                    geom.x,
-                    geom.y
-                ]
-            }
-        })
+    points_geojson = [to_geojson(point) for point in points]
 
     return {
         'type': 'FeatureCollection',
         'features': points_geojson
+    }
+
+
+@view_config(route_name='point', renderer='json')
+def point(request):
+    try:
+        # try to cast the given id to an integer
+        id = int(request.matchdict['id'])
+    except ValueError:
+        raise HTTPBadRequest()
+
+    try:
+        # load the point with the given id
+        point = DBSession.query(Point).filter(Point.id == id).one()
+    except NoResultFound:
+        raise HTTPNotFound()
+
+    return to_geojson(point)
+
+
+def to_geojson(point):
+    # convert from WKB to a Shapely geometry
+    geom = to_shape(point.geom)
+
+    return {
+        'type': 'Feature',
+        'properties': {
+            'id': point.id,
+            'title': point.title,
+            'date': str(point.date)
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                geom.x,
+                geom.y
+            ]
+        }
     }
