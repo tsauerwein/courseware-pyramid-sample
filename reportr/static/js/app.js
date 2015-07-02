1fdca71dd569174847ed2df4eb5e849fc4676942
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-07-02 11:39:01 +0200

    Allow to add new points in the ui

--- reportr/static/js/app.js
+++ reportr/static/js/app.js
@@ -29,3 +29,89 @@ var pointLayer = new ol.layer.Vector({
   })
 });
 map.addLayer(pointLayer);
+
+var newPointSource = new ol.source.Vector();
+var newPointLayer = new ol.layer.Vector({
+  source: newPointSource,
+  style: new ol.style.Style({
+    image: new ol.style.Icon({
+      src: 'static/marker-new.png'
+    })
+  })
+});
+map.addLayer(newPointLayer);
+
+var layerFilter = function(layer) {
+  return layer === pointLayer;
+}
+
+// change mouse cursor when over marker
+map.on('pointermove', function(e) {
+  if (e.dragging) {
+    return;
+  }
+  var pixel = map.getEventPixel(e.originalEvent);
+  var hit = map.hasFeatureAtPixel(pixel, layerFilter);
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
+
+var showPoint = function(feature) {
+  $('#new').hide();
+  $('#problem-title').text(feature.get('title'));
+  var description = feature.get('description') === null ? '' : feature.get('description');
+  $('#problem-description').text(description);
+  $('#problem-date').text(new Date(feature.get('date')).toLocaleString());
+  $('#detail').show();
+};
+
+map.on('click', function(evt) {
+  newPointSource.clear();
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature, layer) {
+        return feature;
+      }, undefined, layerFilter);
+  if (feature) {
+    showPoint(feature);
+  } else {
+    // create new
+    $('#detail').hide();
+    $('#new').show();
+    $('#problem-title-input').val('');
+    $('#problem-description-input').val('');
+    newPointSource.clear();
+    var newFeature = new ol.Feature(
+      new ol.geom.Point(evt.coordinate)
+    );
+    newPointSource.addFeature(newFeature);
+
+    $('#new-problem-form').submit(function(event) {
+      event.preventDefault();
+
+      if ($('#problem-title-input').val() == '') {
+        return;
+      }
+
+      var lonLat = ol.proj.toLonLat(evt.coordinate, 'EPSG:3857');
+      var title = $('#problem-title-input').val();
+      var description = $('#problem-description-input').val();
+      var data = {
+        title: title,
+        description: description,
+        lon: lonLat[0],
+        lat: lonLat[1]
+      };
+
+      $.post(URL_POINTS_ADD, data, function(result, status) {
+        if (status === 'success') {
+          newFeature.set('id', result.properties.id);
+          newFeature.set('date', result.properties.date);
+          newFeature.set('title', result.properties.title);
+          newFeature.set('description', result.properties.description);
+          newPointSource.removeFeature(newFeature);
+          pointSource.addFeature(newFeature);
+          showPoint(newFeature);
+        }
+      }, 'json');
+    });
+  }
+});
--- reportr/templates/index.mako
+++ reportr/templates/index.mako
@@ -7,8 +7,34 @@
 </%block>
 
 <div class="row">
-  <div class="col-md-12">
+  <div class="col-md-10">
     <div id="map" class="map"></div>
+  </div>
+  <div class="col-md-2">
+    <h3>Report problems</h3>
+    Click on the map to add a new point
+    <div id="detail">
+      <strong>Selected problem:</strong>
+      <p id="problem-title"></p>
+      <p id="problem-description"></p>
+      <p id="problem-date"></p>
+    </div>
+    <div id="new">
+      <strong>Report new problem:</strong>
+      <form id="new-problem-form">
+        <div class="form-group">
+          <label for="problem-title-input">Title</label>
+          <input type="text" class="form-control input-sm" id="problem-title-input"
+            name="title" placeholder="e.g. Broken Street-light">
+        </div>
+        <div class="form-group">
+          <label for="problem-title-description">Description</label>
+          <textarea class="form-control input-sm" rows="3" id="problem-description-input"
+            name="description"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary btn-sm" id="">Submit</button>
+      </form>
+    </div>
   </div>
 </div>
 
@@ -17,6 +43,7 @@
 
     <script type="text/javascript">
     var URL_POINTS = "${request.route_url('points')}";
+    var URL_POINTS_ADD = "${request.route_url('point_add')}";
     </script>
     <script src="${request.static_url('reportr:static/js/app.js')}"></script>
 </%block>
