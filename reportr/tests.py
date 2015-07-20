5b5f2c7ee760bebccbc3ea3164012484cb2b8e6d
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-07-20 21:53:51 +0200

    Add basic test

--- reportr/tests.py
+++ reportr/tests.py
@@ -1,55 +1,28 @@
 import unittest
-import transaction
 
 from pyramid import testing
 
 from .models import DBSession
 
 
-class TestMyViewSuccessCondition(unittest.TestCase):
+class TestViews(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
         from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
-
-    def tearDown(self):
-        DBSession.remove()
-        testing.tearDown()
-
-    def test_passing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'reportr')
-
-
-class TestMyViewFailureCondition(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
+        engine = create_engine('postgresql://www-data:www-data@localhost:5432/test')
         DBSession.configure(bind=engine)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
-    def test_failing_view(self):
-        from .views import my_view
+    def test_points(self):
+        from .views import points
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info.status_int, 500)
+        geojson = points(request)
+
+        self.assertTrue(isinstance(geojson, dict))
+        self.assertEqual(geojson['type'], 'FeatureCollection')
+
+        features = geojson['type']
+        self.assertTrue(len(features) > 0)
